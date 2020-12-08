909f03e58fe986888472163a2d94307d12b44913
Dustin Rohde <dustin.rohde@gmail.com>
2020-12-07 21:32:11 -0500

    comment

--- lib/conway/utils.ex
+++ lib/conway/utils.ex
@@ -24,27 +24,27 @@ defmodule Conway.HelpFormatter do
     word_len = String.length(word)
 
     if line_len + 1 + word_len > width do
+      # Adding `word` to `line` would exceed `width`.
       if word_len > width do
+        # `word` is too long to fit on a single line, so we have to break it.
         chars_left = width - line_len - 2
 
         if chars_left >= 2 do
+          # There's enough room to start the break on this line.
           {left, right} = String.split_at(word, chars_left)
-
-          assemble_lines(
-            [right | rest],
-            [indent <> join_word(line, left) <> "-" | lines],
-            "",
-            width,
-            indent
-          )
+          line = indent <> join_word(line, left) <> "-"
+          assemble_lines([right | rest], [line | lines], "", width, indent)
         else
+          # There isn't enough room to start the break, so start a new line with the broken word.
           {left, right} = String.split_at(word, width - 1)
           assemble_lines([right | rest], [indent <> line | lines], left <> "-", width, indent)
         end
       else
+        # `word` will fit on a new line, so start the next line with it.
         assemble_lines(rest, [indent <> line | lines], word, width, indent)
       end
     else
+      # There's room, so add `word` to `line`.
       assemble_lines(rest, lines, join_word(line, word), width, indent)
     end
   end
